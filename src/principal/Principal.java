5402ea6546cc8b1dfa4e9412306cd78699e08b4b
German Gonzales <german.gonzales91@gmail.com>
2024-07-16 13:37:13 -0300

    Initial commit

---
+++ src/principal/Principal.java
@@ -0,0 +1,9 @@
+package principal;
+
+import menu.MenuPrincipal;
+
+public class Principal {
+    public static void main(String[] args) {
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+    }
+}
